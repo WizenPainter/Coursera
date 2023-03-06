787d815b4090111235555b581ef91805b73506cd
Wizen <guzmanpjaime@gmail.com>
2023-03-06 12:25:34 -0600

    Added a booking system with transaction

---
+++ Meta_DBA/db-capstone-project/Booking_system.sql
@@ -0,0 +1,55 @@
+/*Script to insert data into the tables*/
+
+USE LittleLemonDB;
+-- Bookings table
+INSERT INTO Bookings(BookingID, Date, TableNo, CustomerID) VALUES (1, '2022-10-10',5, 1), 
+(2, '2022-11-12', 3, 3), (3, '2022-10-11', 2, 2), (4, '2022-10-13',2, 1);
+
+INSERT INTO Customers(CustomerID, FirstName, LastName) VALUES
+(1, 'Manuel', 'Gomez'),(2, 'Maria', 'Scotch'),(3, 'Jessica', 'Windorf');
+
+/*Stored procedure used to check wheter a table is booked on a certain day*/
+DELIMITER //
+CREATE PROCEDURE CheckBookings(IN checkDay DATE, checkTable INT)
+BEGIN
+	SELECT CASE
+		WHEN checkTable = (SELECT TableNo FROM Bookings WHERE Date = checkDay) THEN CONCAT("Table ", checkTable, " is already booked")
+        ELSE CONCAT("Table ", checkTable, " is not booked")
+	END
+    FROM Bookings;
+END //
+DELIMITER ;
+
+CALL CheckBookings("2022-11-12", 3);
+
+/*Stored procedure that check for availability of a table. It starts a transaction that is rollbacked when the table is already
+booked on the same day. To check with the example from coursera it is necessary to insert a new data field*/
+
+INSERT INTO Bookings(BookingID, Date, TableNo, CustomerID) VALUES(5, "2022-12-17",6, 3);
+
+DELIMITER //
+CREATE PROCEDURE AddValidBooking(IN bookDate DATE, IN bookTable INT)
+BEGIN
+	START TRANSACTION;
+		SELECT @validity :=
+        CASE
+			WHEN bookTable = (SELECT TableNo FROM Bookings WHERE Date = bookDate) THEN CONCAT("Table ", bookTable, " is already booked")
+			ELSE CONCAT("Table ", bookTable, " is not booked")
+		END
+        FROM Bookings;
+		INSERT INTO Bookings(BookingID, Date, TableNo, CustomerID) VALUES (6, bookDate, bookTable, 3);
+        IF @validity = CONCAT("Table ", bookTable, " is already booked") THEN 
+			ROLLBACK;
+            SELECT CONCAT("Table ", bookTable, " is already booked - booking cancelled");
+        ELSE 
+			COMMIT;
+            SELECT CONCAT("Table ", bookTable, " is not booked");
+		END IF;
+END //
+DELIMITER ;
+
+DROP Procedure AddValidBooking;
+
+
+CALL AddValidBooking("2022-12-17",6);
+SELECT * FROM Bookings;
